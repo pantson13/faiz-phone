f792c90d3f3b51bd357b1753b84d17c88a89ecda
pantson13 <pga9156@gmail.com>
2026-07-28 15:30:43 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "faiz-pwa-v26";
+const CACHE_NAME = "faiz-pwa-v27";
 
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./assets/ui.jpg",
+  "./assets/ready-metal.m4a",
   "./assets/s-ready.m4a",
   "./assets/qj.m4a",
   "./assets/exceed charge2.m4a",
